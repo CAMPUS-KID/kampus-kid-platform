43a6aad77a05d231c53cd0485a3c110d660ef988
Jhonatan Torres <jtorresf@unal.edu.co>
2022-10-13 08:01:37 -0500

    fix: admin app config

---
+++ app-campus-kid-admin/src/modules/shared/screens/HomeScreen/HomeScreen.style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const ExampleDiv = styled.div`
+    background: white;
+`;
+
+export { ExampleDiv };
---
+++ app-campus-kid-admin/src/modules/shared/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ExampleDiv } from './HomeScreen.style';
+
+const HomeScreen = () => {
+  return (
+    <ExampleDiv/>
+  );
+}
+
+export default HomeScreen;
