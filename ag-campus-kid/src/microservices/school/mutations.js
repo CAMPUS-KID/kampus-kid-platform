9dbad234661fc66ef82f4eec01c73b0a2dde6ee8
nmejiab <nmejiab@unal.edu.co>
2022-10-15 16:04:16 -0500

    add career-faculty-input-and-get: ag-campus-kid

--- ag-campus-kid/src/microservices/school/mutations.js
+++ ag-campus-kid/src/microservices/school/mutations.js
@@ -6,6 +6,12 @@ const baseUrl = process.env.MS_SCHOOL_BASE_URL;
 
 module.exports = {
   createSite: async (root, { data }) => {
-    return await HttpProvider.post(`${baseUrl}/sites`, data);
+    return await HttpProvider.post(`${baseUrl}/api/sites`, data);
+  },
+  createFaculty: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/api/faculties`, data);
+  },
+  createCareer: async (root, { data }) => {
+    return await HttpProvider.post(`${baseUrl}/api/careers`, data);
   }
 };
--- ag-campus-kid/src/microservices/school/queries.js
+++ ag-campus-kid/src/microservices/school/queries.js
@@ -6,6 +6,12 @@ const baseUrl = process.env.MS_SCHOOL_BASE_URL;
 
 module.exports = {
   getSites: async () => {
-    return await HttpProvider.get(`${baseUrl}/sites`);
+    return await HttpProvider.get(`${baseUrl}/api/sites`);
   },
+  getFaculties: async () => {
+    return await HttpProvider.get(`${baseUrl}/api/faculties`);
+  },
+  getCareers: async () => {
+    return await HttpProvider.get(`${baseUrl}/api/careers`);
+  }
 };
