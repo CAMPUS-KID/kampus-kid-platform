653e480e0297325dd7343a0a8e1e749da98f29c6
Nicolas Hoyos <nhoyosp@unal.edu.co>
2022-10-23 09:22:01 -0500

    some fixes for campus-kid-student-app
    I had to change some files in order to run the app

--- app-campus-kid-student/i18n.ts
+++ app-campus-kid-student/i18n.ts
@@ -3,6 +3,7 @@ import { initReactI18next } from 'react-i18next';
 import en from '@locales/en.json';
 
 i18n.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
   fallbackLng: 'en',
   ns: ['common'],
   defaultNS: 'common',
