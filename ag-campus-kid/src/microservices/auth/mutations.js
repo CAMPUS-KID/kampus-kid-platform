b6405a7a6ce8ad91415510c4bb2218d30331bb04
Nicolás <nmejiab@unal.edu.co>
2022-11-09 22:30:28 -0500

    fix: role for auth mutations

--- ag-campus-kid/src/microservices/auth/mutations.js
+++ ag-campus-kid/src/microservices/auth/mutations.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const { HttpProvider } = require("../../providers");
+const { RoleEnum } = require("../../constants");
+const { RequestPermissions } = require("../../providers/permissions");
 
 const baseUrl = process.env.MS_AUTH_BASE_URL;
 
@@ -8,8 +10,8 @@ module.exports = {
   login: async (root, { data }) => {
     return await HttpProvider.post(`${baseUrl}/auth/login`, data);
   },
-  createUser: async (root, { data }) => {
-    console.log(root)
+  createUser: async ({ currentUser }, { data }) => {
+    RequestPermissions(currentUser, [RoleEnum.ADMIN]);
     return await HttpProvider.post(`${baseUrl}/users`, data);
   },
 };
