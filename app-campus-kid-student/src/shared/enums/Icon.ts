c1cd0a8084109785614ea146965c2ce85b4e4a14
Jhonatan Torres Forero <jtorresf@unal.edu.co>
2022-11-14 16:14:25 -0500

    deploy

---
+++ app-campus-kid-student/src/shared/enums/Icon.ts
@@ -0,0 +1,6 @@
+export enum IconEnum {
+  MOVIES,
+  SERIES,
+  BOOK,
+  ACCOUNT
+}
---
+++ app-campus-kid-student/src/shared/enums/index.ts
@@ -0,0 +1,4 @@
+export * from './Navigation';
+export * from './Icon';
+export * from './Role';
+export * from './StorageKey';
